d657366bbe7c9f288f47c6d21dab64f2350b794a
znffna[Laptop] <znffna@gmail.com>
2024-05-18 16:16:37 +0900

    달력 디자인 변경
    좌측에 밀어넣음.
    달력 날짜 변경기능 추가

--- MainGUI.py
+++ MainGUI.py
@@ -9,9 +9,8 @@ import calendar
 width = 1280
 height = 720
 
-year = 2024
-month = 5
-
+x_position = 20
+y_position = 20
 
 
 class MainGUI:
@@ -20,8 +19,8 @@ class MainGUI:
         month_data = {  # 'MIME Type': 'application/x-www-form-urlencoded; charset=UTF-8',
             'leId': 1,
             'srIdList': '0,9,6',
-            'seasonId': year,
-            'gameMonth': str(month).zfill(2),
+            'seasonId': self.year,
+            'gameMonth': str(self.month).zfill(2),
             'teamID': ''
         }
         res = requests.post('https://www.koreabaseball.com/ws/Schedule.asmx/GetScheduleList', data=month_data)
@@ -82,65 +81,124 @@ class MainGUI:
     def setCalendar(self, frame):
         weekdays = ('일', '월', '화', '수', '목', '금', '토')
         calendar.setfirstweekday(calendar.SUNDAY)
-        cal = calendar.monthcalendar(year, month)
+        cal = calendar.monthcalendar(self.year, self.month)
+
+        if self.CalendarButton:
+            for week in self.CalendarButton:
+                for day in week:
+                    day.grid_remove()
+            self.CalendarButton.clear()
+
+        for week_num, week in enumerate(cal):
+            self.CalendarButton.append([])
+            for day_num, day in enumerate(week):
+                # 버튼 생성
+                btn = Button(frame, text='', width=5, height=5)
+                btn.grid(row=week_num, column=day_num, padx=(x_position, 0), pady=(y_position, 0))
+                btn.grid_remove()
+                self.CalendarButton[week_num].append(btn)
+
         for week_num, week in enumerate(cal):
             for day_num, day in enumerate(week):
-                x_position = 20
                 # x_position = 20 * day_num
                 if day != 0:
                     # 버튼의 x 위치를 요일에 따라 계산
                     # 버튼의 y 위치를 주차에 따라 계산
-                    y_position = 20
                     # y_position = 50 + 50 * week_num
                     button_text = str(day) + '\n'
                     for match in self.month_match[day]:
                         button_text = button_text + match[1] + '\n'
                         # button_text = button_text + match[1] + '\n'
                     # 버튼 생성
-                    btn = Button(frame, text=button_text, width=15, height=6)
-                    btn.grid(row=week_num, column=day_num, padx=(x_position, 0), pady=(y_position, 0))
+                    self.CalendarButton[week_num][day_num]['text'] = button_text
+                    self.CalendarButton[week_num][day_num].grid()
+                    # btn = Button(frame, text=button_text, width=15, height=6)
+                    # btn.grid(row=week_num, column=day_num, padx=(x_position, 0), pady=(y_position, 0))
 
                     # 버튼에 요일 이름 표시
                 if week_num == 0:
                     label = Label(frame, text=weekdays[day_num])
                     label.grid(row=0, column=day_num, padx=(x_position, 0), sticky="n")
 
-        # for day in self.month_match:
-        #     print(day[0])
-        #     row = weekdays.index(day[0][-2])
-        #     button = Button(frame, text = day, width=5, height=2)
-        #     button.grid(row =row)
-        #
 
+    def decreaseYear(self):
+        self.year -= 1
+        self.readMonthData()
+        self.setCalendar(self.달력프레임)
+        self.selectedYM['text'] = str(self.year) + "년 " + str(self.month) + "월"
 
-        # Label(frame, text='페이지1의 내용',
-        #       fg='red', font='helvetica 48').pack(side=TOP)
-        # Label(frame, text='blah blah',
-        #       fg='red', font='helvetica 48').pack(side=TOP)
-        pass
+    def decreaseMonth(self):
+        if self.month == 1:
+            self.year -= 1
+            self.month = 12
+        else:
+            self.month -= 1
+        self.readMonthData()
+        self.setCalendar(self.달력프레임)
+        self.selectedYM['text'] = str(self.year) + "년 " + str(self.month) + "월"
+
+    def increaseMonth(self):
+        if self.month == 12:
+            self.year += 1
+            self.month = 1
+        else:
+            self.month += 1
+        self.readMonthData()
+        self.setCalendar(self.달력프레임)
+        self.selectedYM['text'] = str(self.year) + "년 " + str(self.month) + "월"
+
+    def increaseYear(self):
+        self.year += 1
+        self.readMonthData()
+        self.setCalendar(self.달력프레임)
+        self.selectedYM['text'] = str(self.year) + "년 " + str(self.month) + "월"
 
     def __init__(self):
         window = Tk()
         window.title('국내 축구 경기 일정')
-        notebook = tkinter.ttk.Notebook(window, width=width, height=height)
-        notebook.pack()
 
+        # 달력 생성
+        self.CalendarButton = []
+        Calendar_frame = Frame(window)
+        Calendar_frame.pack(side=LEFT)
+
+        self.year = 2024
+        self.month = 5
+
+        self.달력프레임 = Frame(Calendar_frame)
+        self.달력프레임.pack(side=TOP)
         self.readMonthData()
-        frame1 = Frame(window)
-        notebook.add(frame1, text='경기 일정')
-        self.setCalendar(frame1)
+        self.setCalendar(self.달력프레임)
 
-        frame2 = Frame(window)
+        cFrame2 = Frame(Calendar_frame)
+        cFrame2.pack(side=TOP)
+        Button(cFrame2, text='◀◀', command=self.decreaseYear).pack(side=LEFT)
+        Button(cFrame2, text='◀', command=self.decreaseMonth).pack(side=LEFT)
+        self.selectedYM = Label(cFrame2, text=str(self.year) + "년 " + str(self.month) + "월")
+        self.selectedYM.pack(side=LEFT)
+        Button(cFrame2, text='▶', command=self.increaseMonth).pack(side=LEFT)
+        Button(cFrame2, text='▶▶', command=self.increaseYear).pack(side=LEFT)
+
+        # Notebook 생성
+        세부정보 = Frame(window)
+        세부정보.pack()
+        notebook = tkinter.ttk.Notebook(세부정보, width=width, height=height)
+        notebook.pack(side=LEFT)
+
+        frame1 = Frame(세부정보)
+        notebook.add(frame1, text='경기 일정')
+
+        frame2 = Frame(세부정보)
         notebook.add(frame2, text='날씨 정보')
         Label(frame2, text='페이지2의 내용',
               fg='blue', font='helvetica 48').pack()
 
-        frame3 = Frame(window)
+        frame3 = Frame(세부정보)
         notebook.add(frame3, text='경기장')
         Label(frame3, text='페이지3의 내용',
               fg='orange', font='helvetica 48').pack()
 
-        frame4 = Frame(window)
+        frame4 = Frame(세부정보)
         notebook.add(frame4, text='즐겨찾기 관리')
         Label(frame4, text='페이지4의 내용',
               fg='yellow', font='helvetica 48').pack()
